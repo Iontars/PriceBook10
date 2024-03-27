fac3f05e84dc07c812b73a2fef559998141cc871
Граница миров <fmycry1@gmail.com>
2024-03-27 15:27:39 +0700

    1

--- Chapter_1/Program.cs
+++ Chapter_1/Program.cs
@@ -2,10 +2,30 @@
 {
     internal class Program
     {
+        protected Person person = new(1000,null);
         static void Main(string[] args)
         {
+            Program program = new Program();
             Console.WriteLine();
+            Console.WriteLine(program.person.Name);
+            program.person.Name = "Den";
+            program.OtherMethot();
+        }
 
+        private void OtherMethot()
+        {
+            Console.WriteLine(person.Name);
+        }
+    }
+
+    class Person
+    {
+        public int Money { get; set; }
+        public string Name { get; set; }
+        public Person(int money, string? name)
+        {
+            Money = money;
+            Name = name ?? "Default";
         }
     }
 }
